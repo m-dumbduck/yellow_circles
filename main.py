632be9d0a5edbd6d021fa22cc37151e36412f408
m-dumbduck <maxilomik@yandex.ru>
2022-02-02 19:55:13 +0300

    All colors circles done

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import random
 import sys
 
-from PyQt5 import uic
+from UI import Ui_MainWindow as mV
 from PyQt5.QtGui import QPainter, QColor, QBrush
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 NUMBER_OF_CIRCLES = 10
 WIDTH, HEIGHT = 800, 600
 MAX_SIZE = 100
-COLOR = QColor('yellow')
 
 
-class Example(QMainWindow):
+class Example(QMainWindow, mV):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.setWindowTitle('Git и желтые окружности')
         self.do_paint = False
         self.btn.clicked.connect(self.paint)
@@ -35,8 +34,12 @@ class Example(QMainWindow):
             self.repaint()
 
     def draw_flag(self, qp):
-        qp.setBrush(QBrush(COLOR))
         for _ in range(NUMBER_OF_CIRCLES):
+            qp.setBrush(QBrush(QColor(
+                random.randint(0, 255),
+                random.randint(0, 255),
+                random.randint(0, 255)
+            )))
             size = random.randint(0, MAX_SIZE)
             qp.drawEllipse(random.randint(0, WIDTH), random.randint(0, HEIGHT),
                            size, size)
